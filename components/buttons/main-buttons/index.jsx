2c3242bae46e96b704fcf04f7bfb4bf75ff9dba8
Dimas Maulana <masjamad2@gmail.com>
2024-05-25 23:45:20 +0700

    update dashboard admin & user, main and secondary button, create contact page

--- components/buttons/main-buttons/index.jsx
+++ components/buttons/main-buttons/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function MainButton({text,type = 'button',onClick}) {
+export default function MainButton({ text, type = 'button', onClick }) {
   return (
-    <button className="border border-black px-5 py-3 rounded bg-violet-900 text-white" type={type} onClick={onClick}>{text}</button>
+    <button className="border border-black px-5 py-3 rounded bg-violet-900 hover:bg-violet-800 text-white" type={type} onClick={onClick}>{text}</button>
   )
 }
