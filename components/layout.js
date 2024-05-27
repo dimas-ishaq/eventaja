7db5d8e8e9b5cc40a6c74099ee498b8c7a9bdb32
aqmalProjek <aqmalprojek2001@gmail.com>
2024-05-27 22:38:43 +0700

    fix layout admin & user

--- components/layout.js
+++ components/layout.js
@@ -1,22 +1,17 @@
-import { Inter } from 'next/font/google';
-import '../utils/styles/globals.css';
-import Navbar from '@/components/navbar';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata = {
-  title: 'Event aja',
-  description:
-    'Event aja adalah website untuk menghubungkan ketiga belah pihak Event, Diantaranya Pemilik Event, Penyedian jasa dan terakhir Sponsor. Dengan even aja diharapkan ketiga belah pihak lebih mudah terhubung',
-};
-
+import SideNav from "./dashboard/sidenav-admin"; 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
-      <body className={inter.className + ' h-[100vh]'}>
+      <body className="">
         <main>
-          <Navbar />
-          {children}
+          <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+            <div className="w-full flex-none md:w-64">
+              <SideNav />
+            </div>
+            <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
+              {children}
+            </div>
+          </div>
         </main>
       </body>
     </html>
