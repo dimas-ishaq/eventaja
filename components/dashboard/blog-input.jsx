8940b75fd29c6659dc849bc29743417444c61569
Dimas Maulana <masjamad2@gmail.com>
2024-05-29 20:58:09 +0700

    update dashboard admin/blog

---
+++ components/dashboard/blog-input.jsx
@@ -0,0 +1,54 @@
+
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+export default function BlogInput() {
+  const [value, setValue] = useState('');
+
+  return (
+    <div className='flex flex-col'>
+      <div className="rounded-sm shadow-default">
+        <div className="flex flex-col gap-5.5 p-6.5 mt-2.5">
+          <div>
+            <label className="mb-3 block text-sm font-medium text-black">
+              Judul
+            </label>
+            <input
+              type="text"
+              placeholder="Masukkan Judul Blog/Artikel"
+              className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-violet-500 active:border-violet-500 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-violet-500"
+            />
+          </div>
+
+          <div className='mt-2.5'>
+            <label className="mb-3 block text-sm font-medium text-black">
+              Tags
+            </label>
+            <input
+              type="text"
+              placeholder="Masukkan Tag Blog/Artikel"
+              className="w-full rounded-lg border-[1.5px] bg-transparent px-5 py-3 text-black outline-none transition focus:border-violet-500 active:border-violet-500 disabled:cursor-default disabled:bg-whiter dark:bg-form-input"
+            />
+          </div>
+          <div className='mt-2.5'>
+            <label className="mb-3 block text-sm font-medium text-black">
+              Upload Gambar
+            </label>
+            <input
+              type="file"
+              className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:px-5 file:py-3 file:hover:bg-violet-500 file:hover:bg-opacity-10 focus:border-violet-500 active:border-violet-500 disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark"
+            />
+          </div>
+        </div>
+        <div className="mt-5">
+          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </div>
+        <button type='button' className='px-2 py-1 bg-violet-600 hover:bg-violet-500 mt-5 font-medium text-white rounded-md'>
+          Buat Postingan
+        </button>
+      </div>
+
+    </div>
+  )
+}
--- components/dashboard/sidenav-users.jsx
+++ components/dashboard/sidenav-users.jsx
@@ -20,6 +20,11 @@ const links = [
     href: '/dashboard/user/sponsor',
     icon: 'IoPeopleSharp',
   },
+  {
+    name: 'Transaksi',
+    href: '/dashboard/user/sponsor',
+    icon: 'IoPeopleSharp',
+  },
 ];
 export default function SideNav() {
   return (
@@ -37,9 +42,9 @@ export default function SideNav() {
         <NavLinks links={links} />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
-          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-violet-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
 
-            <div className="hidden md:block">Sign Out</div>
+            <div className="hidden md:block">Log Out</div>
           </button>
         </form>
       </div>
